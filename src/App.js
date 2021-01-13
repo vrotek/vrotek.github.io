7b52e4e164ac0360c210341f1d79968896639aa1
vrotek <42045188+vrotek@users.noreply.github.com>
2021-01-13 21:15:00 +0100

    created project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+import './App.css';
+
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hey its working</h1>
+        <h2>🌵</h2>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
